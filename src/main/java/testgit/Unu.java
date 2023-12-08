4185ab1566207ddc1fb73f428dc608f29957fdff
Dragos Tanta <dragostanta@Dragoss-MacBook-Air.local>
2023-12-08 19:55:30 +0200

    master 6

--- src/main/java/testgit/Unu.java
+++ src/main/java/testgit/Unu.java
@@ -5,7 +5,7 @@ public class Unu {
 	
 	public void testceva() {
 		
-		//linie adaugata pe master nr1
+		//linie adaugata pe master 
 		//linie adaugata pe master nr2
 		//linie adaugata pe branch nr1
 		//linie adaugata pe master nr3
